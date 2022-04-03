58eeaf9704839c876638f0c36b4053e9ea02179c
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-04 02:03:47 +0300

    HW6
    Tasks 9 to 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+const widgetColor = document.querySelector(".widget");
+
+function onChangeColorClick(event) {
+  
+  const bgColor = getRandomHexColor()
+
+  colorText.textContent = bgColor;
+
+  widgetColor.style.backgroundColor = bgColor;
+  
+}
+
+changeColorBtn.addEventListener('click', onChangeColorClick);
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,47 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+const boxes = document.querySelectorAll("#boxes");
+
+const createBtn = document.querySelector('.create');
+const destroyBtn = document.querySelector('.destroy');
+
+createBtn.addEventListener('click', onCreateBtnClick)
+destroyBtn.addEventListener('click',onDestroyBtnClick)
+
+function onCreateBtnClick() {
+  const amount = +input.value;
+
+  createBoxes(amount);
+}
+
+function onDestroyBtnClick() {
+  boxes.innerHTML = '';
+}
+
+
+function createBoxes (amount) {
+
+    console.log(amount);
+  const basicSize = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    
+    const size = basicSize + i * 10;
+    const div = document.createElement('div');
+    
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: #${getRandomHexColor()}`;
+    fragment.appendChild(div);
+    
+  }
+
+  console.log(fragment);
+  boxes.appendChild(fragment);
+  
+}
+
+
+
